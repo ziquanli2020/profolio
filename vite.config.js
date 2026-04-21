e74f95dd9687699469896b7df3ab4653af9dbf31
ZiquanLi <ziquanli2020@gmail.com>
2026-04-21 17:20:04 +0300

    change base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "/profolio"),
     },
   },
 }));
